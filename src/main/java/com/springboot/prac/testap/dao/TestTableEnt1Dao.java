7ba4a608d34066d780177785b0900a7bd9a2e242
nkchiu <nkchiu418@gmail.com>
2021-12-04 10:19:50 +0800

    repository specification

--- src/main/java/com/springboot/prac/testap/dao/TestTableEnt1Dao.java
+++ src/main/java/com/springboot/prac/testap/dao/TestTableEnt1Dao.java
@@ -2,9 +2,10 @@ package com.springboot.prac.testap.dao;
 
 import com.springboot.prac.testap.entity.TestTableEnt1;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TestTableEnt1Dao extends JpaRepository<TestTableEnt1, Integer> {
+public interface TestTableEnt1Dao extends JpaRepository<TestTableEnt1, Integer>, JpaSpecificationExecutor<TestTableEnt1> {
 
 }
