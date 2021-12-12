b1675b36caa5f07f8d8f7d3bb55697f44136cce9
nkchiu <nkchiu418@gmail.com>
2021-12-12 10:53:10 +0800

    增加顯示 啟動資訊

--- src/main/java/com/springboot/prac/PracApplication.java
+++ src/main/java/com/springboot/prac/PracApplication.java
@@ -1,42 +1,52 @@
 package com.springboot.prac;
 
-import com.springboot.prac.config.TestDataSourceConfig;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.env.EnvironmentPostProcessor;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.core.env.*;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
+import java.net.InetAddress;
 import java.util.Arrays;
-import java.util.Properties;
 
 @Slf4j
 @SpringBootApplication
-public class PracApplication implements CommandLineRunner {
-
-
-	@Autowired
-	private Environment env;
+public class PracApplication {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(PracApplication.class);
-		app.run(args);
+		Environment env = app.run(args).getEnvironment();
+		appInfo(env);
+	}
+
+	public static void appInfo(Environment env){
+		String appName = env.getProperty("spring.application.name");
+		String activeProfiles = Arrays.toString(env.getActiveProfiles());
+		String http = "http://";
+		String localDomain = "localhost:";
+		String ip = getIpAddress();
+		String port = env.getProperty("server.port");
+		String path = env.getProperty("server.servlet.context-path");
+
+		log.info("\n============spring boot start=======================\n\t" +
+				"Application " + appName + " is running! \n\t" +
+				"Active profiles: " + activeProfiles + "\n\t" +
+				"Access URLs:\n\t" +
+				"Local: \t\t\t" + http + localDomain + port + path + "\n\t" +
+				"External: \t\t" + http + ip + ":" + port + path + "\n\t" +
+				"swagger-ui: \t" + http + localDomain + port + path + "/swagger-ui.html" + "\n" +
+				"============spring boot end============================");
+
 	}
 
 
-	@Override
-	public void run(String... args) throws Exception {
-		log.info("Active profiles: " + Arrays.toString(env.getActiveProfiles()));
+	private static String getIpAddress(){
+		try {
+			return InetAddress.getLocalHost().getHostAddress();
+		}catch (Exception e){
+			log.error("", e);
+			return null;
+		}
 	}
+
 
 }
